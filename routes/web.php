54342252ab5988a97cbf9529307686b813683451
Asyfdzaky <asyifadzakymaulanaaditria@mail.ugm.ac.id>
2024-10-08 14:54:47 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\productController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+Route::get('/products', [productController::class,"index"])->name('products.index');
+Route::get('/products/create', [productController::class, 'create'])->name('products.create');
+Route::post('/products/store', [productController::class, 'store'])->name('products.store');
+Route::get('/products/show/{product}', [productController::class, 'show'])->name('products.show');
+Route::get('/products/edit/{product}', [productController::class, 'edit'])->name('products.edit');
+Route::post('/products/update/{product}', [productController::class, 'update'])->name('products.update');
+Route::delete('/products/destroy/{product}', [productController::class, 'destroy'])->name('products.destroy'); 
